366c59034d7cd5dd26aa2386a597f223c81e65f3
KOJO MBEAH MENSAH <kmensah29@gmail.com>
2016-09-02 15:22:09 +0000

    initial commit
    Initial commit with front end

---
+++ CRUDProject/form.php
@@ -0,0 +1,149 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>REGISTRATION FORM</title>
+<meta charset="utf-8">
+<meta name = "format-detection" content = "telephone=no" />
+
+<link rel="icon" href="images/favicon.ico">
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="stylesheet" href="css/stuck.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/touchTouch.css">
+<link rel="stylesheet" href="booking/css/booking.css">
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="js/script.js"></script> 
+<script src="js/superfish.js"></script>
+<script src="js/jquery.equalheights.js"></script>
+<script src="js/jquery.mobilemenu.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/tmStickUp.js"></script>
+<script src="js/jquery.ui.totop.js"></script>
+<script src="js/touchTouch.jquery.js"></script>
+<script src="booking/js/booking.js"></script>
+<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" href="css/style1.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+
+
+
+
+</head>
+
+<body class="page1" id="top">
+
+
+<div class=""><form action="" method="POST">
+<fieldset>
+		<div class="top">
+			<h1 id="title" class="hidden"><span id="logo"><span></span></span></h1>
+		</div>
+		<div class="login-box animated">
+			<div class="box-header">
+				<h2>Student Registration</h2>
+			</div>
+			<label for="username">First Name</label>
+			<br/>
+			<input type="text" id="firstname" name='firstname'>
+			<br/>
+			<label for="name">Last Name</label>
+			<br/>
+			<input type="text" id="lastname" name='lastnamename'>
+			<br/>
+			<label for="date">Date Of Birth</label>
+			<br/>
+			<input type="date" id="date" name='date'>
+			<br/>
+			<label for="ID">Program Level</label>
+			<br/>
+			<input type="number" id="number" name='number'>
+			<br/>
+			<label for="email">Phone Number</label>
+			<br/>
+			<input type="text" id="text" name='text'>
+			<br/>
+			<label for="address">Email Address</label>
+			<br/>
+			<input type="text" id="email" name='email'>
+			<br/>
+			<label for="postal address">Residential Address</label>
+			<br/>
+			<input type="text" id="residential address" name='residential address'>
+			<br/>
+			<label for="postal address">Postal Address</label>
+			<br/>
+			<input type="text" id="postal address" name='postal address'>
+			<br/><br/>
+			<label for="postal address">Next of Kin</label>
+			<br/><br/>
+			<label for="username">Name</label>
+			<br/>
+			<input type="text" id="firstname" name='firstname'>
+			<br/>
+			<label for="email">Phone Number</label>
+			<br/>
+			<input type="text" id="text" name='text'>
+			<br/>
+			<label for="address">Email Address</label>
+			<br/>
+			<input type="text" id="email" name='email'>
+			<br/>
+			<label for="postal address">Relationship</label>
+			<br/>
+			<input type="text" id="text" name='text'>
+			<br/>
+			<label for="postal address">Address Line 1</label>
+			<br/>
+			<input type="text" id="address1" name='address1'>
+			<br/>
+			<label for="postal address">Address Line 2</label>
+			<br/>
+			<input type="text" id="address2" name='address2'>
+			<br/>
+			<label for="country">Country</label>
+			<br/>
+			<input type="text" id="country" name='country'>
+			<br/>
+			
+			
+			
+			<button type="submit">Submit</button>
+			<br/>
+			
+		</div>
+	</fieldset></form></div>
+	
+	<div class=""><form action="form.php" method="GET">
+<fieldset>
+		<div class="top">
+			<h1 id="title" class="hidden"><span id="logo"><span></span></span></h1>
+		</div>
+		<div class="login-box animated">
+			<div class="box-header">
+				<h2>Student Search</h2>
+			</div>
+			<legend>Please Type Student Name</legend>
+            <input type="text" id="studentname" name='studentname'>
+            <ul>
+			
+			
+			<input type="submit" value="Search">
+            </ul>
+			
+			
+		</div>
+	</fieldset></form></div>
+	
+
+<br/><br/>
+<br><br><br>
+ </body>
+</html>
